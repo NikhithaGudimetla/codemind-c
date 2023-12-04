fcadb208f2c740e1f568ea2b8763a069680a6168
NikhithaGudimetla <147791809+NikhithaGudimetla@users.noreply.github.com>
2023-12-04 16:49:49 +0530

    #include<stdio.h> int main() {     int n,s,t=0,r;     scanf("%d",&n);     s=n*n;     while(s>0)     {         r=s%10;         t=t+r;         s=s/10;
        }
        if(t==n)
        printf("Neon Number");
        else
        printf("Not Neon Number");
    }

---
+++ Neon_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,s,t=0,r;
+    scanf("%d",&n);
+    s=n*n;
+    while(s>0)
+    {
+        r=s%10;
+        t=t+r;
+        s=s/10;
+        
+    }
+    if(t==n)
+    printf("Neon Number");
+    else
+    printf("Not Neon Number");
+}
