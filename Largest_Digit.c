b9ddfeb0d5218b1ef5bb555e61a03d8c6901bae2
NikhithaGudimetla <147791809+NikhithaGudimetla@users.noreply.github.com>
2023-12-07 09:39:11 +0530

    #include<stdio.h> int main() {     int m,n,i=0;     scanf("%d",&n);     while(n>0)     {         m=n%10;         if(i<m)         {             i=m;         }         n=n/10;     }     printf("%d",i); }

---
+++ Largest_Digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        m=n%10;
+        if(i<m)
+        {
+            i=m;
+        }
+        n=n/10;
+    }
+    printf("%d",i);
+}
