4ef98e90702f8d2b9cbc63412e3433a87df85f43
NikhithaGudimetla <147791809+NikhithaGudimetla@users.noreply.github.com>
2024-01-10 20:29:10 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         printf("%d",j);         for(k=0;k<n-i;k++)         printf("*");         printf(" ");     } }

---
+++ Basic_Pattern_5.4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        printf("%d",j);
+        for(k=0;k<n-i;k++)
+        printf("*");
+        printf("
+");
+    }
+}
